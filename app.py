c2bd14aab30204a43f376b97efda648c34af2a4f
blakelobato <blakelobato1@gmail.com>
2020-02-16 23:55:09 -0800

    added json users page

--- app.py
+++ app.py
@@ -9,7 +9,17 @@ def index():
 
 @app.route("/about")
 def about():
-    return "About me page"
+    return "About Me Page"
+
+@app.route("/users")
+@app.route("/users.json")
+def users():
+    users = [
+        {"id":1, "name":"First"},
+        {"id":2, "name":"Second"},
+        {"id":3, "name":"Third"},
+    ]
+    return jsonify()
 
 ###FLASK_APP=app.py flask run 
 # used to run the app locally
