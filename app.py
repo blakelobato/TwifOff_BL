1da9130cd378ca7bf6dfb2b60d97da2849adbe24
blakelobato <blakelobato1@gmail.com>
2020-02-16 23:50:18 -0800

    about me page

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
     return "Hello World!"
 
+
+@app.route("/about")
+def about():
+    return "About me page"
+
+###FLASK_APP=app.py flask run 
+# used to run the app locally
